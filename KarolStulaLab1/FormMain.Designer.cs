ba4a8a196d38fc19464d92b6ab5f8b074f7ea49a
Karol <karolstula@gmail.com>
2019-04-03 19:27:05 +0200

    added ba1

---
+++ KarolStulaLab1/FormMain.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace KarolStulaLab1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.buttonChangeColor = new System.Windows.Forms.Button();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.timerCounter = new System.Windows.Forms.Timer(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonNewWindow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(832, 310);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Zamknij program";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.labelName.Location = new System.Drawing.Point(1, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(258, 55);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Karol Stuła";
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Location = new System.Drawing.Point(11, 58);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(75, 23);
+            this.buttonChange.TabIndex = 2;
+            this.buttonChange.Text = "Zmień";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.ButtonChange_Click);
+            // 
+            // buttonChangeColor
+            // 
+            this.buttonChangeColor.Location = new System.Drawing.Point(832, 0);
+            this.buttonChangeColor.Name = "buttonChangeColor";
+            this.buttonChangeColor.Size = new System.Drawing.Size(75, 23);
+            this.buttonChangeColor.TabIndex = 3;
+            this.buttonChangeColor.Text = "Zmień kolor";
+            this.buttonChangeColor.UseVisualStyleBackColor = true;
+            this.buttonChangeColor.Click += new System.EventHandler(this.ButtonChangeColor_Click);
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(12, 88);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(120, 20);
+            this.textBoxNumber.TabIndex = 4;
+            this.textBoxNumber.Text = "1";
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(32, 114);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(75, 23);
+            this.buttonShow.TabIndex = 5;
+            this.buttonShow.Text = "Wyświetl";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.ButtonShow_Click);
+            // 
+            // timerCounter
+            // 
+            this.timerCounter.Interval = 1000;
+            this.timerCounter.Tick += new System.EventHandler(this.TimerCounter_Tick);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(491, 114);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // buttonNewWindow
+            // 
+            this.buttonNewWindow.Location = new System.Drawing.Point(10, 237);
+            this.buttonNewWindow.Name = "buttonNewWindow";
+            this.buttonNewWindow.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewWindow.TabIndex = 7;
+            this.buttonNewWindow.Text = "Nowe okno";
+            this.buttonNewWindow.UseVisualStyleBackColor = true;
+            this.buttonNewWindow.Click += new System.EventHandler(this.ButtonChangeObject_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 332);
+            this.Controls.Add(this.buttonNewWindow);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.buttonChangeColor);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonClose);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.Button buttonChangeColor;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.Timer timerCounter;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonNewWindow;
+    }
+}
+
